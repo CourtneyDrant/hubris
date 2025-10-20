5a9b8e48f615cec8391689fe531d4d87d3405365
CourtneyDrant <courtney.drant@amd.com>
2025-10-20 15:30:38 -0700

    Initial build of responder.  Not functional.  Used for prototyping.

--- task/pldm-responder/src/main.rs
+++ task/pldm-responder/src/main.rs
@@ -6,10 +6,10 @@
 #![no_main]
 
 use userlib::*;
-use firmware_device_api::pldm_initiator_task;
 use firmware_device_api::FDError;
 use pldm_lib::cmd_interface::CmdInterface;
 use pldm_lib::transport::MctpTransport;
+use pldm_lib::config::PLDM_PROTOCOL_CAPABILITIES;
 //TODO: We need an inteface to loop on with IPC into firmware-device-api
 //use firmware_device_api::
 
@@ -17,10 +17,11 @@ pub const MAX_MCTP_PLDM_MSG_SIZE: usize = 1024;
 
 #[export_name = "main"]
 fn main() -> ! {
-    //let pldm_responder_task = pldm_initiator_task {
-        //cmd_interface: &CmdInterface::new(MctpTransport::new()),
-    //};
+    let mut transport = MctpTransport::new();
+    let mut cmd_interface = CmdInterface::new(&PLDM_PROTOCOL_CAPABILITIES,&mut transport);
+    let mut msg_buff = [0u8; MAX_MCTP_PLDM_MSG_SIZE];
     loop {
+        let _ = cmd_interface.handle_responder_msg(&mut msg_buff);
         /*
         // TODO: Add PLDM message processing loop
         // IPC to handle message if PLDM
