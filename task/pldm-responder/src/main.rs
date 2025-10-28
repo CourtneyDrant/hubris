3b742f2027cf09aa5ccac28bcd351811820debae
CourtneyDrant <courtney.drant@amd.com>
2025-10-27 20:57:23 -0700

    Updated pldm-responder IPC objects and methods.

--- task/pldm-responder/src/main.rs
+++ task/pldm-responder/src/main.rs
@@ -13,9 +13,10 @@
 
 use mctp::{Eid, Listener, MsgType, RespChannel};
 use userlib::*;
-use fw_device_api::FwDevice;
+use fw_device_api::{FwDevice, FDError};
 
 task_slot!(MCTP, mctp_server);
+task_slot!(FW_DEVICE, fw_device);
 
 #[export_name = "main"]
 fn main() -> ! {
@@ -25,17 +26,40 @@ fn main() -> ! {
     let mut listener = stack.listener(MsgType(1), None).unwrap_lite();
     let mut recv_buf = [0; 255];
 
-    let mut fw_device = FwDevice::new();
+    // Create FwDevice client from task ID
+    let fw_device = FwDevice::from(FW_DEVICE.get_task_id());
 
     loop {
         let (_, _, msg, mut resp) = listener.recv(&mut recv_buf).unwrap_lite();
 
-        match resp.send(msg) {
-            Ok(_) => {
-                fw_device.process_request(&msg, &mut []).ok();
+        // Process PLDM message through FwDevice
+        let mut response_buf = [0u8; 255]; // Buffer for response
+        
+        match fw_device.process_request(msg, &mut response_buf) {
+            Ok(response_len) => {
+                // Send the processed response back via MCTP
+                let response = &response_buf[..response_len];
+                match resp.send(response) {
+                    Ok(_) => {}
+                    Err(_e) => {
+                        // Error sending response to peer
+                    }
+                }
             }
-            Err(_e) => {
-                // Error sending response to peer
+            Err(FDError::ServerRestarted) => {
+                // FW Device server restarted, may need to handle this
+                // For now, send original message as echo
+                match resp.send(msg) {
+                    Ok(_) => {}
+                    Err(_e) => {}
+                }
+            }
+            Err(_other_error) => {
+                // Other FW Device errors, send error response or echo
+                match resp.send(msg) {
+                    Ok(_) => {}
+                    Err(_e) => {}
+                }
             }
         }
     }
